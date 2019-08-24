56e2cb9374347a736bce276fe331abf105ebc452
LzxJane <924304321@qq.com>
2019-08-24 15:24:03 +0800

    lesson-1

---
+++ src/components/Count.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="count">
+    <div>
+      <hr />
+      <h1>Count.vue:{{count}}</h1>
+      <p>{{todos}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "count",
+  components: {},
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+// vuex的实例
 import store from './store'
 
 Vue.config.productionTip = false
